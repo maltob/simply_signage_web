758d5f16b503845dca9c5da0d0cfff4bf5a11614
--set <--set>
2025-01-10 20:33:12 -0500

    Base application

---
+++ Options/IdentityOptions.cs
@@ -0,0 +1,12 @@
+public class IdentityOptions {
+    public const string Identity = "Identity";
+
+    public bool AllowLocalRegistration {get; set;} = false;
+
+    public string MicrosoftClientID {get; set;} = String.Empty;
+    public string MicrosoftClientSecret {get; set;} = String.Empty;
+
+    
+    public string GoogleClientID {get; set;} = String.Empty;
+    public string GoogleClientSecret {get; set;} = String.Empty;
+}
